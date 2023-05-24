af1cee9535cb799c28e439f2c4cd46bcb226db89
tqtensor <victortang212@gmail.com>
2023-05-24 14:28:26 +0000

    feat: add praat change gender method

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-import argparse
 import json
-import datetime as dt
-import numpy as np
-import matplotlib.pyplot as plt
-import IPython.display as ipd
-from tqdm import tqdm
-from scipy.io.wavfile import write
-import soundfile as sf
-
-import torch
-
-# For Grad-TTS
-import params
-from model import GradTTS
-from text import text_to_sequence, bndict
-from text.symbols import symbols
-from utils import intersperse
 
 # For HiFi-GAN
 import sys
-sys.path.append('./hifi-gan/')
+
+import soundfile as sf
+import torch
+
+# For Grad-TTS
+import TTS.params as params
+from TTS.model import GradTTS
+from TTS.praat_utils import change_gender
+from TTS.text import bndict, text_to_sequence
+from TTS.text.symbols import symbols
+from TTS.utils import intersperse
+
+sys.path.append("./TTS/hifi-gan/")
 from env import AttrDict
 from models import Generator as HiFiGAN
 
@@ -29,16 +24,31 @@ if torch.cuda.is_available():
 else:
     device = "cpu"
 
+
 def load_acoustic_model(chkpt_path, lex_path):
-    generator = GradTTS(len(symbols)+1, 1, params.spk_emb_dim,
-                    params.n_enc_channels, params.filter_channels,
-                    params.filter_channels_dp, params.n_heads, params.n_enc_layers,
-                    params.enc_kernel, params.enc_dropout, params.window_size,
-                    params.n_feats, params.dec_dim, params.beta_min, params.beta_max,
-                    pe_scale=1000).to(device)
-    generator.load_state_dict(torch.load(chkpt_path, map_location=lambda loc, storage: loc))
+    generator = GradTTS(
+        len(symbols) + 1,
+        1,
+        params.spk_emb_dim,
+        params.n_enc_channels,
+        params.filter_channels,
+        params.filter_channels_dp,
+        params.n_heads,
+        params.n_enc_layers,
+        params.enc_kernel,
+        params.enc_dropout,
+        params.window_size,
+        params.n_feats,
+        params.dec_dim,
+        params.beta_min,
+        params.beta_max,
+        pe_scale=1000,
+    ).to(device)
+    generator.load_state_dict(
+        torch.load(chkpt_path, map_location=lambda loc, storage: loc)
+    )
     _ = generator.eval()
-    print(f'Number of parameters: {generator.nparams}')
+    print(f"Number of parameters: {generator.nparams}")
 
     cmu = bndict.BNDict(lex_path)
     return generator, cmu
@@ -48,7 +58,9 @@ def load_vocoder(chkpt_path, config_path):
     with open(config_path) as f:
         h = AttrDict(json.load(f))
     hifigan = HiFiGAN(h).to(device)
-    hifigan.load_state_dict(torch.load(chkpt_path, map_location=lambda loc, storage: loc)['generator'])
+    hifigan.load_state_dict(
+        torch.load(chkpt_path, map_location=lambda loc, storage: loc)["generator"]
+    )
     _ = hifigan.eval()
     hifigan.remove_weight_norm()
 
@@ -56,28 +68,44 @@ def load_vocoder(chkpt_path, config_path):
 
 
 def infer(text, generator, dct):
-    x = torch.LongTensor(intersperse(text_to_sequence(text, dictionary=dct), len(symbols))).to(device)[None]
+    x = torch.LongTensor(
+        intersperse(text_to_sequence(text, dictionary=dct), len(symbols))
+    ).to(device)[None]
     x_lengths = torch.LongTensor([x.shape[-1]]).to(device)
 
-    _, y_dec, _ = generator.forward(x, x_lengths, n_timesteps=50, temperature=1.3,
-                                        stoc=False, spk=None,
-                                        length_scale=0.91)
+    _, y_dec, _ = generator.forward(
+        x,
+        x_lengths,
+        n_timesteps=50,
+        temperature=1.3,
+        stoc=False,
+        spk=None,
+        length_scale=0.91,
+    )
 
     return y_dec
 
-generator, dct = load_acoustic_model('./logs/bahnar_exp/grad_1344.pt', './data/bahnar_lexicon.txt')
-generator_fm, dct_fm = load_acoustic_model('./logs/bahnar_female_exp/grad_1250.pt', './data/bahnar_lexicon.txt')
 
-hifigan = load_vocoder('./checkpts/hifigan.pt', './checkpts/hifigan-config.json')
+generator, dct = load_acoustic_model(
+    "./TTS/logs/bahnar_exp/grad_1344.pt", "./TTS/data/bahnar_lexicon.txt"
+)
+generator_fm, dct_fm = load_acoustic_model(
+    "./TTS/logs/bahnar_female_exp/grad_1250.pt", "./TTS/data/bahnar_lexicon.txt"
+)
+
+hifigan = load_vocoder(
+    "./TTS/checkpts/hifigan.pt", "./TTS/checkpts/hifigan-config.json"
+)
 output_sampling_rate = 22050
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     input_text = text = "trong glong tôjroh ameêm teh ñak"
-    output_path = 'test.wav'
+    output_path = "test.wav"
 
     y = infer(input_text, generator, dct)
 
     with torch.no_grad():
         audio = hifigan.forward(y).cpu().squeeze().clamp(-1, 1)
 
+    audio = change_gender(audio, output_sampling_rate, 75, 600, 1.1, 0.0, 1.0, 1.0)
     sf.write(output_path, audio, output_sampling_rate)
